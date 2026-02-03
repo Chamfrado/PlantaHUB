39b60eda7a0c93321187006113ba6989908ae2eb
Chamfrado <lohran.chamfrado@outlook.com>
2026-02-02 22:26:29 -0300

    feat: add Spring Boot API starter project

---
+++ apps/api/plantahub-api/src/main/java/com/plantahub/api/PlantahubApiApplication.java
@@ -0,0 +1,13 @@
+package com.plantahub.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantahubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantahubApiApplication.class, args);
+	}
+
+}
---
+++ apps/api/plantahub-api/src/test/java/com/plantahub/api/PlantahubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantahub.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantahubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
